0b788e0057bacf6fca28ad8c458be9a964eb2d92
Aron Zwaan <aronzwaan@gmail.com>
2023-11-16 12:01:41 +0100

    Generic Scope Graph trait

---
+++ scopegraphs/src/scopegraphs/scopegraphs.rs
@@ -0,0 +1,15 @@
+use std::slice::Iter;
+
+pub trait ScopeGraph {
+    type Scope;
+    type Label;
+    type Data;
+
+    fn add_scope(&self, data: Self::Data) -> &Self::Scope;
+
+    fn add_edge(&self, src: &Self::Scope, lbl: &Self::Label, dst: &Self::Scope);
+
+    fn get_data(&self, scope: &Self::Scope) -> &Self::Data;
+
+    fn get_edges(&self, scope: &Self::Scope, lbl: &Self::Data) -> Iter<'_, Self::Scope>;
+}
