bcb2b13523cc9481f5cd1633814c85be194ff5b2
jdonszelmann <jonabent@gmail.com>
2023-11-24 16:13:59 +0100

    ref iter

--- scopegraphs-lib/src/label.rs
+++ scopegraphs-lib/src/label.rs
@@ -2,4 +2,8 @@ pub trait Label: Copy + Eq {
     fn iter() -> impl Iterator<Item = Self>
     where
         Self: Sized;
+
+    fn iter_ref() -> impl Iterator<Item = &'static Self>
+    where
+        Self: Sized + 'static;
 }
