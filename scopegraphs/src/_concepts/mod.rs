11a4827788e6273c840a059b833c2d040792d94a
jdonszelmann <jonabent@gmail.com>
2024-04-19 15:34:56 +0200

    concepts

---
+++ scopegraphs/src/_concepts/mod.rs
@@ -0,0 +1,1 @@
+//! # Concepts of Scopegraphs
--- scopegraphs/src/lib.rs
+++ scopegraphs/src/lib.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
 pub use scopegraphs_lib::*;
 pub use scopegraphs_macros::*;
 pub use scopegraphs_regular_expressions::*;
+
+#[cfg(feature = "unstable-doc")]
+pub mod _concepts;
