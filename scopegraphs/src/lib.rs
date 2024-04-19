049daeb2737e4047bcb27059ff0f9f74ea9bf41d
jdonszelmann <jonabent@gmail.com>
2024-04-19 16:06:48 +0200

    nightly docs and justfile

--- scopegraphs/src/lib.rs
+++ scopegraphs/src/lib.rs
@@ -1,4 +1,9 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+//! A scope graph is an abstraction over name resolution.
+//!
+//!
+//! * [Explanation of Concepts used in ScopeGraphs](crate::_concepts)
+//!
+#![cfg_attr(any(RUSTC_IS_NIGHTLY, docsrs), feature(doc_auto_cfg, doc_cfg))]
 
 pub use scopegraphs_lib::*;
 pub use scopegraphs_macros::*;
