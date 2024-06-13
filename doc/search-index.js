eba0711bf76ca9634bc277450d3e455308b8a680
Aron Zwaan <aronzwaan@gmail.com>
2024-06-13 14:31:31 +0200

    Remove doc/ files

--- doc/search-index.js
+++
@@ -1,5 +0,0 @@
-var searchIndex = JSON.parse('{\
-"traits":{"doc":"","t":"QQQIKKKK","n":["Data","Label","Scope","ScopeGraph","add_edge","add_scope","get_data","get_edges"],"q":[[0,"traits"]],"d":["Type of data associated with nodes.","Type of edge labels.","Type of nodes.","Scope Graph operations.","Adds a new edge from <code>src</code>, to <code>dst</code>, with label <code>lbl</code> to the …","Adds a new scope to the graph, with <code>data</code> as its associated …","",""],"i":[2,2,2,0,2,2,2,2],"f":[0,0,0,0,[[]],[[]],[[]],[[],1]],"c":[],"p":[[3,"Iter"],[8,"ScopeGraph"]]}\
-}');
-if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
-if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
--- doc/src-files.js
+++
@@ -1,4 +0,0 @@
-var srcIndex = JSON.parse('{\
-"traits":["",[],["traits.rs"]]\
-}');
-createSrcSidebar();
