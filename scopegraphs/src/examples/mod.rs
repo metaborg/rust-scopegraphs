0f3dbec421ae6fdc8517a102df809da6ba04d8c8
jdonszelmann <jonabent@gmail.com>
2024-09-12 16:34:17 +0200

    .

--- scopegraphs/src/examples/mod.rs
+++ scopegraphs/src/examples/mod.rs
@@ -5,7 +5,7 @@ use scopegraphs_render_docs::render_scopegraphs;
 /// # Common patterns in scope graphs
 pub mod patterns;
 
-#[render_scopegrahs]
+#[render_scopegraphs]
 /// An example of a small language with records (structs) and name resolution of fields.
 ///
 // {tutor name "records-example-full"}
